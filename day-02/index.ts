a20ed983cb40f00c38eddfd6c53d3c8280341721
Jack Thorpe <jack.thorpe@airelogic.com>
2025-12-02 14:11:07 +0000

    Day 2 part 2

--- day-02/index.ts
+++ day-02/index.ts
@@ -4,19 +4,35 @@ const data = readFileSync("./input.txt", "utf-8");
 
 var ranges = data.split(",").map((x) => x.split("-"));
 
-console.log(ranges);
+const matches = (input: string, splitPoint: number): boolean => {
+  if (input.length % splitPoint !== 0) {
+    return false;
+  }
+  const splits: string[] = [];
 
-const total = ranges.reduce((total, range) => {
+  let index = 0;
+  while (index < input.length) {
+    splits.push(input.slice(index, index + splitPoint));
+    index += splitPoint;
+  }
+
+  const matches = splits.every((x) => x === splits[0]);
+  return matches;
+};
+
+let total = ranges.reduce((total, range) => {
   const start = +range[0];
   const end = +range[1];
 
   let sum = 0;
 
   for (let i = start; i <= end; i++) {
-    let value = i.toString();
+    const value = i.toString();
+
     if (value.length % 2 !== 0) {
       continue;
     }
+
     let parts = [
       value.slice(0, value.length / 2),
       value.slice(value.length / 2),
@@ -29,4 +45,28 @@ const total = ranges.reduce((total, range) => {
   return total + sum;
 }, 0);
 
-console.log(total)
+console.log(total);
+
+total = ranges.reduce((total, range) => {
+  const start = +range[0];
+  const end = +range[1];
+
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    const value = i.toString();
+    let splitPoint = 1;
+
+    while (splitPoint <= value.length / 2) {
+      if (matches(value, splitPoint)) {
+        sum += i;
+        break;
+      }
+      splitPoint++;
+    }
+  }
+
+  return total + sum;
+}, 0);
+
+console.log(total);
